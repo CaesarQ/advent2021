a76d010e439b4230c9ec8da1b9100db5935a9d65
Giuseppe Castiglione <g.m.a.castiglione@gmail.com>
2021-12-06 21:50:52 -0500

    O(n) time solution to day 6

--- days/06/day06.jl
+++ days/06/day06.jl
@@ -1,4 +1,19 @@
 using DelimitedFiles
 
+function bincount(arr::Matrix{Int}, minlength::Int)
+    ret = zeros(Int64, minlength)
+    for k in arr
+        ret[k] += 1
+    end
+    return ret 
+end
+
+
 # Load data
 input = readdlm("input.txt", ',', Int, '\n');
+# Initial population
+p0 = bincount(input .+ 1, 9)
+T = readdlm("transitions.txt", ',', Int, '\n')
+
+println("Population after 80 days: $(sum((T^80) * p0))")
+println("Population after 256 days: $(sum((T^256) * p0))")
