ae1d927d53c558c4b93114894dd49e70ca7ef931
Giuseppe Castiglione <g.m.a.castiglione@gmail.com>
2021-12-10 02:19:14 -0500

    day 8 solutions

--- days/08/day08.jl
+++ days/08/day08.jl
@@ -1,20 +1,42 @@
-function parse_input(input_src::AbstractString)
-    raw_input = open(input_src) do file
-        read(file, String)
-    end
+using DelimitedFiles
 
-    splits = filter(x -> ~isempty(x), split(raw_input, r"\n| \| "))
+#=
+|--------------------A NEAT TRICK------------------------------------------|
+signals is a list of the randomly permuted segments of each digit, presented 
+in a random order.  However, we can still use this to decode one of the
+output digits, without necessarily having to learn the permutation itself.
 
-    signals = splits[1:2:end]
-    outputs = splits[2:2:end]
+For a given digit, we simply count how many times each segment 
+appears in the signal list.  This gives a unique value to each digit that is
+invariant under permutations.
 
-    return signals, outputs
+Let us construct an encoder matrix, whose rows are the segments, and whose 
+columns are the digits.  The matrix contains a '1' when a segment is used in a 
+particular digit, and a '0' when it is not.  Call this matrix E.  Then the 
+following vector is covvariant under permutations P:
 
+v = E^T E|1>
+v -> v' = Pv
+
+We can then determine P using elementwise comparisons as P = (v .== v)
+
+=#
+
+E = readdlm("encoder.txt", ',', Int, '\n')
+unique_num = sum(transpose(E) * E, dims=1)[1, :]
+mapping = Dict(unique_num .=> 0:9)
+
+occurences(str1, str2) = sum([str1...].∈str2)
+
+function decode(line)
+    segments, output = split(line, " | ")
+    return ["$(mapping[occurences(segments, o)])" for o in split(output)] |> join
 end
 
-signals, outputs = parse_input("input.txt")
+outputs = map(decode, readlines("input.txt"))
 
-count_1478(x) = sum(map(x -> x in [2,3,4,7], map(length, split(x, " "))))
+search_str = "1478"
+counts = sum(map(x -> occurences(x, search_str), outputs))
 
-
-println("Numer of times (1,4,7,8) appears: $(sum(count_1478.(outputs)))")
+println("Numer of times (1,4,7,8) appears: $(counts)")
+println("Sum of outputs: $(sum(map(x -> parse(Int, x), outputs)))")
